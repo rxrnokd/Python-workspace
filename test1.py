b5c3b918e0b5dc69bb871d1e351f2f8be1f6e703
rxrnokd <gooteagong@gmail.com>
2023-11-22 17:57:59 +0900

    first commit

--- test1.py
+++ test1.py
@@ -1,2 +1,15 @@
-
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        return self.first + self.second
+    def sub(self):
+        return self.first - self.second
+    def mul(self):
+        return self.first * self.second
+a = FourCal(1, 2)
+print(a.add())
+        
+        
 
