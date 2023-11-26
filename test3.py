fd56ac5bb47294674e66cd4e407c9653d2b4c770
rxrnokd <gooteagong@gmail.com>
2023-11-27 08:56:23 +0900

    first commit

--- test3.py
+++ test3.py
@@ -5,5 +5,5 @@ print(today)
 c = '12'
 print(c.rjust(2, '0'))
 print(str(today.year)+str(today.month)+str(today.day), str(today.hour).rjust(2, '0')+'00')
-a, b = short_term_situation(str(today.year)+str(today.month)+str(today.day), str(today.hour)+'00')
+a, b = short_term_situation(str(today.year)+str(today.month)+str(today.day), str(today.hour).rjust(2, '0')+'00')
 print(a)
