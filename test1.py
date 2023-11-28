50d8a57a6f30e71c9b8da1239dbfe48debf8836d
rxrnokd <gooteagong@gmail.com>
2023-11-28 22:34:09 +0900

    first commit

--- test1.py
+++ test1.py
@@ -7,6 +7,7 @@ first_weekday = dt.date(today.year, today.month, 1).weekday()
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 temp, rain = short_term_situation(str(today.year)+str(today.month)+str(today.day), str(today.hour).rjust(2, '0')+'00')
 
+
 if today.weekday() == 0:
     weekday = '월요일'
 elif today.weekday() == 1:   
@@ -38,8 +39,11 @@ root.configure(bg='beige')
 frame_today_weather = Frame(root, width=root_width, height=40, relief='solid', bd=1, bg='beige')
 frame_today_weather.grid(row=0, column=0, columnspan=7)
 
-label_today_weather = Label(frame_today_weather, text=str(today.year) + '년 ' + str(today.month) + '월 ' + str(today.day) + '일 '+ weekday +' 부산광역시 현재기온: ' + temp + ' ' + rain, font=('Arial',10), bg='beige')
+label_today_weather = Label(frame_today_weather, text=str(today.month) + '월 ' + str(today.day) + '일 '+ weekday +' 부산광역시 현재기온: ' + temp + ' ' + rain, font=('Arial',10), bg='beige')
 label_today_weather.place(x = 0, y = 8)
+
+now_date = Label(root, width=5, height=2, text='2023년 11월', bg='beige', font=('Arial', 10))
+now_date.grid(row=1, column=0, columnspan=7, sticky=N+E+W+S)
 
 mo_label = Label(root, width=5, height=2, text='월', bg='beige', font=('Arial',10))
 tu_label = Label(root, width=5, height=2, text='화', bg='beige', font=('Arial',10))
@@ -48,19 +52,19 @@ th_label = Label(root, width=5, height=2, text='목', bg='beige', font=('Arial',
 fr_label = Label(root, width=5, height=2, text='금', bg='beige', font=('Arial',10))
 sa_label = Label(root, width=5, height=2, text='토', bg='beige', font=('Arial',10))
 su_label = Label(root, width=5, height=2, text='일', bg='beige', font=('Arial',10))
-mo_label.grid(row=1, column=0, sticky=N+E+W+S)
-tu_label.grid(row=1, column=1, sticky=N+E+W+S)
-we_label.grid(row=1, column=2, sticky=N+E+W+S)
-th_label.grid(row=1, column=3, sticky=N+E+W+S)
-fr_label.grid(row=1, column=4, sticky=N+E+W+S)
-sa_label.grid(row=1, column=5, sticky=N+E+W+S)
-su_label.grid(row=1, column=6, sticky=N+E+W+S)
+mo_label.grid(row=2, column=0, sticky=N+E+W+S)
+tu_label.grid(row=2, column=1, sticky=N+E+W+S)
+we_label.grid(row=2, column=2, sticky=N+E+W+S)
+th_label.grid(row=2, column=3, sticky=N+E+W+S)
+fr_label.grid(row=2, column=4, sticky=N+E+W+S)
+sa_label.grid(row=2, column=5, sticky=N+E+W+S)
+su_label.grid(row=2, column=6, sticky=N+E+W+S)
 
 
 
 day_counter = 1 - first_weekday
 i = 1
-for a in range(2,8):
+for a in range(3,9):
     for b in range(7):
         if 1 <= day_counter <= month_days[today.month]:
             btn = Button(root, text=str(i), width=5, height=2, bg='beige')
