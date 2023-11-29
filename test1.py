3a51d9f599dbd2ee037dba4e3311c1518cf7108e
rxrnokd <gooteagong@gmail.com>
2023-11-30 06:56:26 +0900

    first commit

--- test1.py
+++ test1.py
@@ -5,7 +5,7 @@ from weather import *
 
 
 root = Tk()
-root.title('Calendar')
+root.title('달력 프로그램')
 
 root_width = 335
 root_height = 330
@@ -15,7 +15,7 @@ root_pos_y = 250
 root.geometry('{}x{}+{}+{}'.format(root_width, root_height, root_pos_x, root_pos_y)) # 가로 * 세로 + x좌표 + y좌표
 root.resizable(False, False) # x좌표 y좌표 값 변경 불가
 
-root.configure(bg='beige')
+root.configure(bg='skyblue')
 
 def display_custom_calendar(month, year):
     global btn
@@ -31,7 +31,7 @@ def display_custom_calendar(month, year):
     for a in range(3,9):
         for b in range(7):
             if 1 <= day_counter <= month_days[month]:
-                btn = Button(root, text=str(i), width=5, height=2, bg='beige')
+                btn = Button(root, text=str(i), width=5, height=2, bg='skyblue')
                 btn.grid(row=a, column=b, sticky=N+E+W+S)
                 btns.append(btn)
                 i += 1
@@ -71,16 +71,16 @@ elif today.weekday() == 6:
     weekday = '일요일'
 
 
-frame_today_weather = Frame(root, width=root_width, height=40, relief='solid', bd=1, bg='beige')
+frame_today_weather = Frame(root, width=root_width, height=40, relief='solid', bd=1, bg='skyblue')
 frame_today_weather.grid(row=0, column=0, columnspan=7)
 
-label_today_weather = Label(frame_today_weather, text=str(today.month) + '월 ' + str(today.day) + '일 '+ weekday +' 부산광역시 현재기온: ' + temp + ' ' + rain, font=('Arial',10), bg='beige')
+label_today_weather = Label(frame_today_weather, text=str(today.month) + '월 ' + str(today.day) + '일 '+ weekday +' 부산광역시 현재기온: ' + temp + ' ' + rain, font=('Arial',10), bg='skyblue')
 label_today_weather.place(x = 0, y = 8)
 
 # change_date = Frame(root, width=root_width, height=40, relief='solid', bd=1, bg='beige' )
 # change_date.grid(row=1, column=0, columnspan=7)
 
-change_date = Label(root, text=str(today.year)+'년 '+str(today.month)+'월', bg='beige', font=('Arial', 10))
+change_date = Label(root, text=str(today.year)+'년 '+str(today.month)+'월', bg='skyblue', font=('Arial', 10))
 change_date.grid(row=1, column=0, columnspan=2)
 
 valuse = [str(i) for i in range(1, 13)]
@@ -91,18 +91,18 @@ month_combobox.set(str(today.month))
 year_entry = Entry(root, width=5)
 year_entry.grid(row=1, column=4, sticky=N+E+W+S)
 
-enter_btn = Button(root, width=5, text='입력', bg='beige', command=enter_btn_cmd)
+enter_btn = Button(root, width=5, text='입력', bg='skyblue', command=enter_btn_cmd)
 enter_btn.grid(row=1, column=6, sticky=N+E+W+S)
 
 
 
-mo_label = Label(root, width=5, height=2, text='월', bg='beige', font=('Arial',10))
-tu_label = Label(root, width=5, height=2, text='화', bg='beige', font=('Arial',10))
-we_label = Label(root, width=5, height=2, text='수', bg='beige', font=('Arial',10))
-th_label = Label(root, width=5, height=2, text='목', bg='beige', font=('Arial',10))
-fr_label = Label(root, width=5, height=2, text='금', bg='beige', font=('Arial',10))
-sa_label = Label(root, width=5, height=2, text='토', bg='beige', font=('Arial',10))
-su_label = Label(root, width=5, height=2, text='일', bg='beige', font=('Arial',10))
+mo_label = Label(root, width=5, height=2, text='월', bg='skyblue', font=('Arial',10))
+tu_label = Label(root, width=5, height=2, text='화', bg='skyblue', font=('Arial',10))
+we_label = Label(root, width=5, height=2, text='수', bg='skyblue', font=('Arial',10))
+th_label = Label(root, width=5, height=2, text='목', bg='skyblue', font=('Arial',10))
+fr_label = Label(root, width=5, height=2, text='금', bg='skyblue', font=('Arial',10))
+sa_label = Label(root, width=5, height=2, text='토', bg='skyblue', font=('Arial',10))
+su_label = Label(root, width=5, height=2, text='일', bg='skyblue', font=('Arial',10))
 mo_label.grid(row=2, column=0, sticky=N+E+W+S)
 tu_label.grid(row=2, column=1, sticky=N+E+W+S)
 we_label.grid(row=2, column=2, sticky=N+E+W+S)
