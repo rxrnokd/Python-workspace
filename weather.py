9557fad677c63bea17f3ba846840fa5e15f74747
rxrnokd <gooteagong@gmail.com>
2023-12-08 19:25:53 +0900

    first commit

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ import requests
 def short_term_situation(date, time):
     # 기상청 초단기 실황 url, 요청변수 
     url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
-    params = {'serviceKey' : 'HcYIyLc612fqNjJ%2FNYs252txzi9CLPoCrXJ7odF8baDERTVYVxSOOPmeKEWFkzL88VMMAOlyv8nUgzu9MUauOA%3D%3D', 'pageNo' : '1', 'numOfRows' : '4', 'dataType' : 'JSON', 'base_date' : date, 'base_time' : time, 'nx' : '98', 'ny' : '76' }
+    params = {'serviceKey' : 'HcYIyLc612fqNjJ/NYs252txzi9CLPoCrXJ7odF8baDERTVYVxSOOPmeKEWFkzL88VMMAOlyv8nUgzu9MUauOA==', 'pageNo' : '1', 'numOfRows' : '4', 'dataType' : 'JSON', 'base_date' : date, 'base_time' : time, 'nx' : '98', 'ny' : '76' }
     
     # 데이터 받아옴
     response = requests.get(url, params=params, timeout = None)
